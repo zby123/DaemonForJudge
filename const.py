11919071163bea6f51cf566f5926e66263daddc7
zby <ZengBiyang@sjtu.edu.cn>
2018-09-25 22:54:24 +0800

    add const.py

---
+++ const.py
@@ -0,0 +1,8 @@
+# -*- coding: utf-8 -*-
+
+AI_SAVE_DIRECTORY   = '/home/p2dv/data/ai/'
+AI_SERVER_DIRECTORY = '/home/p2dv/judge_server/'
+SERVER_NAME         = 'Server AliYun'
+TOKEN               = 'I am a token.'
+CORE_SERVER         = 'http://120.79.70.76:4000'
+REQUESTS_TIMEOUT    = 3
